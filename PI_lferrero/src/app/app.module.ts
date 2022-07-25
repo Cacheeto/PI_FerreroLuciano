359d9c9127cd9ddc09d05d66e66b82fbfba39850
Luciano Ferrero <lucianonicolasferrero@gmail.com>
2022-07-25 20:29:16 -0300

    Actualizacion

--- PI_lferrero/src/app/app.module.ts
+++ PI_lferrero/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componente/header/header.component';
+import { LogoAPComponent } from './componente/header/logo-ap/logo-ap.component';
+import { HeroComponent } from './componente/header/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    HeroComponent,
+
   ],
   imports: [
     BrowserModule,
