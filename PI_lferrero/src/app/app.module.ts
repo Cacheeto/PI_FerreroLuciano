58d7960a6dff3c3ebaa85c73832a36b5390b2c09
Luciano Ferrero <lucianonicolasferrero@gmail.com>
2022-08-13 15:09:59 -0300

    Se agregaron varias seciones

--- PI_lferrero/src/app/app.module.ts
+++ PI_lferrero/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componente/header/header.component';
 import { LogoAPComponent } from './componente/header/logo-ap/logo-ap.component';
 import { HeroComponent } from './componente/header/hero/hero.component';
+import { AboutmeComponent } from './componente/header/aboutme/aboutme.component';
+import { ContentComponent } from './componente/content/content.component';
+import { ExperienceComponent } from './componente/content/experience/experience.component';
+import { EducationComponent } from './componente/content/education/education.component';
+import { ProgressBarComponent } from './componente/content/progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +18,11 @@ import { HeroComponent } from './componente/header/hero/hero.component';
     HeaderComponent,
     LogoAPComponent,
     HeroComponent,
+    AboutmeComponent,
+    ContentComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ProgressBarComponent,
 
   ],
   imports: [
